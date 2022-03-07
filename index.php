da9decc1ca30ede7c27bf019c98e6ee11a2abdea
xcyrax <rival_b@yahoo.com>
2022-03-07 17:20:03 +0700

    menambahkan dua list baru di index.php

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
   <h1>Projek Kedua Belajar Git</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde doloribus fuga minus inventore necessitatibus, similique magni dignissimos quo id rem nihil eos ut laboriosam harum deleniti nostrum modi minima amet. Porro iste voluptas earum eligendi iure quia facilis, ut. Autem ducimus doloremque tempora at in dolores minus voluptate deleniti id.</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
 </body>
 </html>
